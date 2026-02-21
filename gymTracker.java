5df7b4c779ae25e595c61dbcc880eb2ff82c2001
YaBoyJams <jamesinforo@gmail.com>
2026-02-21 16:09:26 +0000

    Complete Version

--- gymTracker.java
+++ gymTracker.java
@@ -1,82 +1,154 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.Scanner;;
 
 public class gymTracker {
     public static void main(String[] args) {
         // Initialise the scanner
         Scanner scanner = new Scanner(System.in);
-        // Will loop user until they stop logging workouts
+        // Will loop user until they stop logging workouts and if to quite the program
+        // or not
         boolean continueLogging = true;
-        // Ask user if they want to log a workout or view previous workout
-        System.out.print("Do you want to log a workout (Y) or view previously logged workout (N): ");
-        String logInput = scanner.nextLine().trim().toUpperCase();
-        if (logInput.equals("Y") || logInput.equals("YES")) {
-            do {
-                // User will be asked of the workout was a PR attempt
-                boolean pr = false;
-                // Used to repeat the question if the user inputs an invalid answer
-                boolean validInput = false;
+        boolean quit = false;
+        do {
+            // User will be asked of the workout was a PR attempt
+            boolean pr = false;
+            // Ask user if they want to log a workout or view previous workout
+            System.out.print("Do you want to log a workout (Y) or view previously logged workout (N): ");
+            String logInput = scanner.nextLine().trim().toUpperCase();
+            if (logInput.equals("Y") || logInput.equals("YES")) {
                 do {
-                    System.out.print("Did you hit a PR? (Y/N): ");
-                    String prInput = scanner.nextLine().trim().toUpperCase();
-                    if (prInput.equals("Y") || prInput.equals("YES")) {
-                        pr = true;
-                        validInput = true;
-                    } else if (prInput.equals("N") || prInput.equals("NO")) {
-                        pr = false;
-                        validInput = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter 'Y' for yes or 'N' for no. ");
-                        validInput = false;
+                    // Used to repeat the question if the user inputs an invalid answer
+                    boolean validInput = false;
+                    do {
+                        System.out.print("Did you hit a PR? (Y/N): ");
+                        String prInput = scanner.nextLine().trim().toUpperCase();
+                        if (prInput.equals("Y") || prInput.equals("YES")) {
+                            pr = true;
+                            validInput = true;
+                        } else if (prInput.equals("N") || prInput.equals("NO")) {
+                            pr = false;
+                            validInput = true;
+                        } else {
+                            System.out.println("Invalid input. Please enter 'Y' for yes or 'N' for no. ");
+                            validInput = false;
+                        }
+                    } while (!validInput);
+                    // If the workout was a PR attempt then no sets will be asked for. The user will
+                    // be asked for the workout name, reps, and weight. If it wasn't a PR attempt
+                    // then it will also ask for the number of sets.
+                    if (pr) {
+                        System.out.print("Enter the name of the exercise: ");
+                        String workoutName = scanner.nextLine().trim().toUpperCase();
+                        System.out.print("Enter the number of reps: ");
+                        int reps = scanner.nextInt();
+                        System.out.print("Enter the weight used (in kg): ");
+                        double weight = scanner.nextDouble();
+                        workoutInfo prWorkout = new workoutInfo(workoutName, reps, weight);
+                        prWorkout.setPR(pr);
+                        System.out.println("Thanks for logging. Here is your workout info: ");
+                        prWorkout.displayWorkoutInfo();
+                        scanner.nextLine(); // Consume the newLine left by nextInt() and nextDouble()
+                        // Writes the logged workout to file saving it for future use
+                        prWorkout.writeWorkoutToFile();
+
+                        // Retrieve previous PR attempt with the same name and display it
+                        prWorkout.readPRWorkoutFromFile("PR attempt", workoutName);
+                        prWorkout.displayPreviousPRWorkout();
+
+                        // Compare the logged workout vs the previous workout
+                        prWorkout.compareWorkouts();
                     }
-                } while (!validInput);
-                // If the workout was a PR attempt then no sets will be asked for. The user will
-                // be asked for the workout name, reps, and weight. If it wasn't a PR attempt
-                // then it will also ask for the number of sets.
-                if (pr) {
-                    System.out.print("Enter the name of the exercise: ");
-                    String workoutName = scanner.nextLine().trim().toUpperCase();
-                    System.out.print("Enter the number of reps: ");
-                    int reps = scanner.nextInt();
-                    System.out.print("Enter the weight used (in kg): ");
-                    double weight = scanner.nextDouble();
-                    workoutInfo prWorkout = new workoutInfo(workoutName, reps, weight);
-                    prWorkout.setPR(pr);
-                    System.out.println("Thanks for logging. Here is your workout info: ");
-                    prWorkout.displayWorkoutInfo();
-                    scanner.nextLine(); // Consume the newLine left by nextInt() and nextDouble()
-                }
-                if (!pr) {
-                    System.out.print("Enter the name of the exercise: ");
-                    String workoutName = scanner.nextLine();
-                    System.out.print("Enter the number of sets: ");
-                    int sets = scanner.nextInt();
-                    System.out.print("Enter the number of reps: ");
-                    int reps = scanner.nextInt();
-                    System.out.print("Enter the weight used (in kg): ");
-                    double weight = scanner.nextDouble();
-                    workoutInfo nonPRWorkout = new workoutInfo(workoutName, reps, weight);
-                    nonPRWorkout.setSets(sets);
-                    nonPRWorkout.setPR(pr);
-                    System.out.println("Thanks for logging. Here is your workout info: ");
-                    nonPRWorkout.displayWorkoutInfo();
-                    scanner.nextLine(); // Consume the newLine left by nextInt() and nextDouble()
-                }
-                System.out.print("Would you like to log another workout? (Y/N): ");
-                String continueInput = scanner.nextLine().trim().toUpperCase();
-                if (continueInput.equals("N") || continueInput.equals("NO")) {
-                    continueLogging = false;
-                }
-            } while (continueLogging);
-        }
-        // Ask user if they want to view previously logged workout
-        if(logInput.equals("N") || logInput.equals("NO")) {
-            System.out.println("Enter if the workout was a PR or not (Y/N), the date of the workout in the format dd/mm/yyyy, and the exercise name: ");
-            
+                    if (!pr) {
+                        System.out.print("Enter the name of the exercise: ");
+                        String workoutName = scanner.nextLine().trim().toUpperCase();
+                        System.out.print("Enter the number of sets: ");
+                        int sets = scanner.nextInt();
+                        System.out.print("Enter the number of reps: ");
+                        int reps = scanner.nextInt();
+                        System.out.print("Enter the weight used (in kg): ");
+                        double weight = scanner.nextDouble();
+                        workoutInfo nonPRWorkout = new workoutInfo(workoutName, reps, weight);
+                        nonPRWorkout.setSets(sets);
+                        nonPRWorkout.setPR(pr);
+                        System.out.println("Thanks for logging. Here is your workout info: ");
+                        nonPRWorkout.displayWorkoutInfo();
+                        scanner.nextLine(); // Consume the newLine left by nextInt() and nextDouble()
+                        // Writes the logged workouut to file saving it for future use
+                        nonPRWorkout.writeWorkoutToFile();
 
-        }
+                        // Retrieve previous non-PR attempt with the same name and display it
+                        nonPRWorkout.readNonPRWorkoutFromFile("Normal working sets", workoutName);
+                        nonPRWorkout.displayPreviousNonPRWorkout();
 
+                        // Compare the logged workout vs the previous workout
+                        nonPRWorkout.compareWorkouts();
+                    }
+                    System.out.print("Would you like to log another workout? (Y/N): ");
+                    String continueInput = scanner.nextLine().trim().toUpperCase();
+                    if (continueInput.equals("N") || continueInput.equals("NO")) {
+                        continueLogging = false;
+                    }
+                } while (continueLogging);
+            }
+            // Code to view previously logged workout, will ask user for workout details
+            if (logInput.equals("N") || logInput.equals("NO")) {
+                String prStatus = "";
+                LocalDate date;
+                DateTimeFormatter inputFmt = DateTimeFormatter.ofPattern("dd/MM/uuuu")
+                        .withResolverStyle(ResolverStyle.STRICT);
+                System.out.println("You will need to enter your previous exercise details to view them.");
+                System.out.print("Did you hit a PR? (Y/N): ");
+                String prInput = scanner.nextLine().trim().toUpperCase();
+                if (prInput.equals("Y") || prInput.equals("YES")) {
+                    pr = true;
+                    prStatus = "PR attempt";
+                } else if (prInput.equals("N") || prInput.equals("NO")) {
+                    pr = false;
+                    prStatus = "Normal working sets";
+                } else {
+                    System.out.println("Invalid input. Please enter 'Y' for yes or 'N' for no. ");
+                }
+                System.out.print("What was the name of the exercise? ");
+                String exerciseName = scanner.nextLine().trim().toUpperCase();
+                System.out.print("Enter the date of the workout (dd/mm/yyyy): ");
+                while (true) {
+                    String dateInput = scanner.nextLine().trim();
+                    try {
+                        date = LocalDate.parse(dateInput, inputFmt);
+                        break;
+                    } catch (DateTimeParseException e) {
+                        System.out.print("Invalid date. Example: 21/02/2026. Enter the date again: ");
+                    }
+                }
+                workoutInfo workoutHistory = new workoutInfo(exerciseName, 0, 0);
+                // Convert to the exact format stored in WorkoutHistory.txt
+                String formattedDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+                // Read the workout details from the file using the values the user inputted
+                workoutHistory.readWorkoutFromFile(prStatus, formattedDate, exerciseName);
+                workoutHistory.displayPreviousWorkout();
+
+            }
+            System.out.println("Do you want to quit the program? (Y/N): ");
+            boolean validInput = false;
+            do {
+                String prInput = scanner.nextLine().trim().toUpperCase();
+                if (prInput.equals("Y") || prInput.equals("YES")) {
+                    validInput = true;
+                    quit = true;
+                } else if (prInput.equals("N") || prInput.equals("NO")) {
+                    validInput = true;
+                    quit = false;
+                } else {
+                    System.out.println("Invalid input. Please enter 'Y' for yes or 'N' for no. ");
+                    validInput = false;
+                }
+            } while (!validInput);
+        } while (!quit);
         System.out.println("Thanks for checking in!");
         scanner.close();
     }
-
 }
--- workoutInfo.java
+++ workoutInfo.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+
+//import java.util.ArrayList;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.io.*;
@@ -10,7 +11,11 @@ public class workoutInfo {
     private double weight;
     private boolean pr = false;
     private LocalDateTime dateOfWorkout = LocalDateTime.now();
-    ArrayList<workoutInfo> workoutHistory = new ArrayList<>();
+    private String workoutDetails = "";
+    private String prWorkoutDetails = "";
+    private String nonPRWorkoutDetails = "";
+    private boolean foundDataMatch = false;
+    // ArrayList<workoutInfo> workoutHistory = new ArrayList<>();
 
     /**
      * Constructor for the workoutInfo class that the user will input the workout
@@ -124,12 +129,14 @@ public class workoutInfo {
 
     // Method to write the current workout information to array list to store the
     // workout history
-    public void logWorkout() {
-        // String workoutDetails = isPR() + ", " + "Date: " + getDateOfWorkout() + ",
-        // Workout Name: " + getWorkoutName() + ", Sets: " + getSets() + ", Reps: " +
-        // getReps() + ", Weight: " + getWeight() + " kg";
-
-    }
+    /**
+     * public void logWorkout() {
+     * // String workoutDetails = isPR() + ", " + "Date: " + getDateOfWorkout() + ",
+     * // Workout Name: " + getWorkoutName() + ", Sets: " + getSets() + ", Reps: " +
+     * // getReps() + ", Weight: " + getWeight() + " kg";
+     * 
+     * }
+     */
 
     // Method to write the current workout to a file to store the workout
     public void writeWorkoutToFile() {
@@ -138,13 +145,13 @@ public class workoutInfo {
         // Formatted so that can be searched by PR then data the workoutName
         try {
             String dataToAdd = "";
-            if(pr) {
-                dataToAdd =isPR() + ", " + "Date: " + getDateOfWorkout() + ", Workout Name: " + getWorkoutName()
-                    + ", Reps: " + getReps() + ", Weight: " + getWeight() + " kg";
+            if (pr) {
+                dataToAdd = isPR() + ", " + "Date: " + getDateOfWorkout() + ", Workout Name: " + getWorkoutName()
+                        + ", Reps: " + getReps() + ", Weight: " + getWeight() + " kg";
             }
-            if(!pr) {
-                dataToAdd =isPR() + ", " + "Date: " + getDateOfWorkout() + ", Workout Name: " + getWorkoutName()
-                    + ", Sets: " + getSets() + ", Reps: " + getReps() + ", Weight: " + getWeight() + " kg";
+            if (!pr) {
+                dataToAdd = isPR() + ", " + "Date: " + getDateOfWorkout() + ", Workout Name: " + getWorkoutName()
+                        + ", Sets: " + getSets() + ", Reps: " + getReps() + ", Weight: " + getWeight() + " kg";
             }
             File file = new File("WorkoutHistory.txt");
 
@@ -162,24 +169,31 @@ public class workoutInfo {
         }
     }
 
-    // Method to read the previously logged workout from the file, individual methods for PR and non-PR used when user enters a new log.
+    // Method to read the previously logged workout from the file, individual
+    // methods for PR and non-PR used when user enters a new log.
     public String readWorkoutFromFile(String prStatus, String dateOfWorkout, String workoutName) {
-        String workoutDetails = "";
         try {
             File file = new File("WorkoutHistory.txt");
+            if (!file.exists()) {
+                return "No previous workouts available.";
+            }
             FileReader fr = new FileReader(file.getName());
             BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            while (line != null) {
-                line = br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
                 String[] workoutData = line.split(", ");
-                if(workoutData[0].equals(prStatus) && workoutData[1].equals("Date: " + dateOfWorkout) && workoutData[2].equals("Workout Name: " + workoutName)) {
+                if (workoutData[0].equals(prStatus) && workoutData[1].equals("Date: " + dateOfWorkout)
+                        && workoutData[2].equals("Workout Name: " + workoutName)) {
                     workoutDetails = line;
+                    foundDataMatch = true;
                     break;
                 }
             }
             br.close();
             fr.close();
+            if (line == null) {
+                return "No matching data found or file does not exist.";
+            }
             System.out.println("Workout successfully read from file.");
         } catch (IOException e) {
             System.out.println("An error occured while reading from the file: " + e.getMessage());
@@ -187,58 +201,166 @@ public class workoutInfo {
         return workoutDetails;
     }
 
-    public String readPRWorkoutFromFile(String prStatus, String dateOfWorkout, String workoutName) {
-        String workoutDetails = "";
+    public String readPRWorkoutFromFile(String prStatus, String workoutName) {
         try {
             File file = new File("WorkoutHistory.txt");
+            if (!file.exists()) {
+                return "No previous workouts available.";
+            }
             FileReader fr = new FileReader(file.getName());
             BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            while (line != null) {
-                line = br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
                 String[] workoutData = line.split(", ");
-                if(workoutData[0].equals(prStatus) && workoutData[1].equals("Date: " + dateOfWorkout) && workoutData[2].equals("Workout Name: " + workoutName)) {
-                    workoutDetails = line;
+                if (workoutData[0].equals(prStatus) && workoutData[2].equals("Workout Name: " + workoutName)) {
+                    prWorkoutDetails = line;
+                    foundDataMatch = true;
                     break;
                 }
             }
             br.close();
             fr.close();
+            if (line == null) {
+                return "No matching data found or file does not exist.";
+            }
             System.out.println("Workout successfully read from file.");
         } catch (IOException e) {
             System.out.println("An error occured while reading from the file: " + e.getMessage());
         }
-        return workoutDetails;
+        return prWorkoutDetails;
     }
 
-    public String readNonPRWorkoutFromFile(String prStatus, String dateOfWorkout, String workoutName) {
-        String workoutDetails = "";
+    public String readNonPRWorkoutFromFile(String prStatus, String workoutName) {
         try {
             File file = new File("WorkoutHistory.txt");
+            if (!file.exists()) {
+                return "No previous workouts available.";
+            }
             FileReader fr = new FileReader(file.getName());
             BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            while (line != null) {
-                line = br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
                 String[] workoutData = line.split(", ");
-                if(workoutData[0].equals(prStatus) && workoutData[1].equals("Date: " + dateOfWorkout) && workoutData[2].equals("Workout Name: " + workoutName)) {
-                    workoutDetails = line;
+                if (workoutData[0].equals(prStatus) && workoutData[2].equals("Workout Name: " + workoutName)) {
+                    nonPRWorkoutDetails = line;
+                    foundDataMatch = true;
                     break;
                 }
             }
             br.close();
             fr.close();
+            if (line == null) {
+                return "No matching data found or file does not exist.";
+            }
             System.out.println("Workout successfully read from file.");
         } catch (IOException e) {
             System.out.println("An error occured while reading from the file: " + e.getMessage());
         }
-        return workoutDetails;
+        return nonPRWorkoutDetails;
     }
 
     // Method to compare the current workout to the previous one and calculate the %
-    // increase in PR weight (and reps if necessary)
+    // increase in PR weight (and reps if necessary) and increase in sets, calucated
+    // if it's grater than, less than, or gives a statement if equal than the
+    // previous workout.
     public void compareWorkouts() {
+        if (pr) {
+            String[] parts = prWorkoutDetails.split(", ");
+            double previousWeight = Double.parseDouble(parts[4].split(" ")[1]);
+            int previousReps = Integer.parseInt(parts[3].split(" ")[1]);
+            if (getWeight() > previousWeight) {
+                double weightIncrease = getWeight() - previousWeight;
+                double weightIncreasePercent = (weightIncrease / previousWeight) * 100;
+                System.out.printf("Weight increase by: %.2f kg\nWeight percentage increase: %.2f%%\n", weightIncrease, weightIncreasePercent);;
+            } else if (getWeight() < previousWeight) {
+                double weightDecrease = previousWeight - getWeight();
+                double weightDecreasepercent = (weightDecrease / previousWeight) * 100;
+                System.out.printf("Weight decreased by: %.2f kg\nWeight percentage decrease: %.2f%%\n", weightDecrease, weightDecreasepercent);
+            } else if (getWeight() == previousWeight) {
+                System.out.println("No increase in weight. Previous weight: " + previousWeight + " kg");
+            } else {
+                System.out.println("Error comparing weight.");
+            }
+            if (getReps() > previousReps) {
+                int repsIncrease = getReps() - previousReps;
+                System.out.println("Reps increase by: " + repsIncrease);
+            } else if (getReps() < previousReps) {
+                int repsDecrease = previousReps - getReps();
+                System.out.println("Reps decreased by: " + repsDecrease);
+            } else if (getReps() == previousReps) {
+                System.out.println("No increase in reps. Previous reps: " + previousReps);
+            } else {
+                System.out.println("Error comparing reps.");
+            }
+        }
+        if (!pr) {
+            String[] parts = nonPRWorkoutDetails.split(", ");
+            double previousWeight = Double.parseDouble(parts[5].split(" ")[1]);
+            int previousReps = Integer.parseInt(parts[4].split(" ")[1]);
+            int previousSets = Integer.parseInt(parts[3].split(" ")[1]);
+            if (getWeight() > previousWeight) {
+                double weightIncrease = getWeight() - previousWeight;
+                double weightIncreasePercent = (weightIncrease / previousWeight) * 100;
+                System.out.printf("Weight increase by: %.2f kg\nWeight percentage increase: %.2f%%\n", weightIncrease, weightIncreasePercent);
+            } else if (getWeight() < previousWeight) {
+                double weightDecrease = previousWeight - getWeight();
+                double weightDecreasepercent = (weightDecrease / previousWeight) * 100;
+                System.out.printf("Weight decreased by: %.2f kg\nWeight percentage decrease: %.2f%%\n", weightDecrease, weightDecreasepercent);
+            } else if (getWeight() == previousWeight) {
+                System.out.println("No increase in weight. Previous weight: " + previousWeight + " kg");
+            } else {
+                System.out.println("Error comparing weight.");
+            }
+            if (getSets() > previousSets) {
+                int setsIncrease = getSets() - previousSets;
+                System.out.println("Set increase: " + setsIncrease);
+            } else if (getSets() < previousSets) {
+                int setsDecrease = previousSets - getSets();
+                System.out.println("Set decrease: " + setsDecrease);
+            } else if (getSets() == previousSets) {
+                System.out.println("No increase in sets. Previous sets: " + previousSets);
 
+            } else {
+                System.out.println("Error comparing reps.");
+            }
+            if (getReps() > previousReps) {
+                int repsIncrease = getReps() - previousReps;
+                System.out.println("Reps increase by: " + repsIncrease);
+            } else if (getReps() < previousReps) {
+                int repsDecrease = previousReps - getReps();
+                System.out.println("Reps decreased by: " + repsDecrease);
+            } else if (getReps() == previousReps) {
+                System.out.println("No increase in reps. Previous reps: " + previousReps);
+            } else {
+                System.out.println("Error comparing reps.");
+            }
+
+        }
     }
 
+    // Methods to print the previous workout details, used to show the user the
+    // previous workout
+    public void displayPreviousWorkout() {
+        if(foundDataMatch) {
+        System.out.println(workoutDetails);
+        } else {
+            System.out.println("No previous workout data found");
+        }
+    }
+
+    public void displayPreviousPRWorkout() {
+        if(foundDataMatch) {
+        System.out.println(prWorkoutDetails);
+        } else {
+            System.out.println("No previous workout data found");
+        }
+    }
+
+    public void displayPreviousNonPRWorkout() {
+        if(foundDataMatch) {
+        System.out.println(nonPRWorkoutDetails);
+        } else {
+            System.out.println("No previous workout data found");
+        }
+    }
 }
